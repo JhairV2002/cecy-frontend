c851fda48d62453b0dd05d65a3f28e0c358568a3
Soy-Anderson <ajr.parra@yavirac.edu.ec>
2023-05-18 06:38:27 -0500

    config

--- src/app/pages/core/user-administration/dashboard/dashboard.component.ts
+++ src/app/pages/core/user-administration/dashboard/dashboard.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '@models/authentication';
+import { AuthService } from '@services/auth';
+import { ChartConfiguration } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  date: Date = new Date();
+  dateFormat: string = '';
+  greetingMessage: string = '';
+  user: any;
+  // Doughnut
+  doughnutChartLabels: any[] = [];
+  doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] =
+    [];
 
-  constructor() { }
+  doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
+    responsive: false,
+  };
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authService.userProfile$.subscribe((user: any) => {
+      console.log('DASHBOARD USER', user[0]);
+      this.user = user[0];
+      this.setGreetingMessage();
+    });
+
+    setInterval(() => {
+      this.date = new Date();
+    }, 1000);
   }
 
+  setGreetingMessage() {
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+
+    if (currentHour < 12) {
+      this.greetingMessage = `Buenos días, ${this.user?.names}`;
+    } else if (currentHour < 18) {
+      this.greetingMessage = `Buenas tardes, ${this.user?.names}`;
+    } else {
+      this.greetingMessage = `Buenas noches, ${this.user?.names}`;
+    }
+  }
 }
--- src/app/shared/components/layouts/sidebar/sidebar.component.ts
+++ src/app/shared/components/layouts/sidebar/sidebar.component.ts
@@ -16,17 +16,23 @@ export class SidebarComponent implements OnInit {
   display = false;
   itemsAdmin: MenuItem[] = [
     {
-      label: 'Admnistrador',
+      label: 'Administrador',
+      icon: 'fa-solid fa-key',
       items: [
         {
           label: 'Dashboard',
-          icon: 'pi pi-chart-pie',
-          routerLink: '/user-administration/dashboard',
+          icon: 'fa-solid fa-house',
+          routerLink: '/administrator/dashboard',
         },
         {
           label: 'Usuarios',
-          icon: 'pi pi-user',
-          routerLink: ['/user-administration/admin'],
+          icon: 'fa-solid fa-users-line',
+          routerLink: ['/administrator/users'],
+        },
+        {
+          label: 'Roles',
+          icon: 'fa-solid fa-users-gear',
+          routerLink: ['/administrator/roles'],
         },
       ],
     },
@@ -111,6 +117,7 @@ export class SidebarComponent implements OnInit {
   closed: boolean = true;
   menu: MenuItem[] = [];
   user: User | null = null;
+  model: MenuItem[] = [];
 
   constructor(
     private menuHttpService: MenuHttpService,
@@ -124,6 +131,189 @@ export class SidebarComponent implements OnInit {
       console.log('SEDEIBAR USER', user[0]);
       this.user = user[0];
     });
+
+    this.model = [
+      {
+        label: 'Home',
+        items: [
+          { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] },
+        ],
+      },
+      {
+        label: 'UI Components',
+        items: [
+          {
+            label: 'Form Layout',
+            icon: 'pi pi-fw pi-id-card',
+            routerLink: ['/uikit/formlayout'],
+          },
+          {
+            label: 'Input',
+            icon: 'pi pi-fw pi-check-square',
+            routerLink: ['/uikit/input'],
+          },
+          {
+            label: 'Float Label',
+            icon: 'pi pi-fw pi-bookmark',
+            routerLink: ['/uikit/floatlabel'],
+          },
+          {
+            label: 'Invalid State',
+            icon: 'pi pi-fw pi-exclamation-circle',
+            routerLink: ['/uikit/invalidstate'],
+          },
+          {
+            label: 'Button',
+            icon: 'pi pi-fw pi-box',
+            routerLink: ['/uikit/button'],
+          },
+          {
+            label: 'Table',
+            icon: 'pi pi-fw pi-table',
+            routerLink: ['/uikit/table'],
+          },
+          {
+            label: 'List',
+            icon: 'pi pi-fw pi-list',
+            routerLink: ['/uikit/list'],
+          },
+          {
+            label: 'Tree',
+            icon: 'pi pi-fw pi-share-alt',
+            routerLink: ['/uikit/tree'],
+          },
+          {
+            label: 'Panel',
+            icon: 'pi pi-fw pi-tablet',
+            routerLink: ['/uikit/panel'],
+          },
+          {
+            label: 'Overlay',
+            icon: 'pi pi-fw pi-clone',
+            routerLink: ['/uikit/overlay'],
+          },
+          {
+            label: 'Media',
+            icon: 'pi pi-fw pi-image',
+            routerLink: ['/uikit/media'],
+          },
+          {
+            label: 'Menu',
+            icon: 'pi pi-fw pi-bars',
+            routerLink: ['/uikit/menu'],
+            routerLinkActiveOptions: {
+              paths: 'subset',
+              queryParams: 'ignored',
+              matrixParams: 'ignored',
+              fragment: 'ignored',
+            },
+          },
+          {
+            label: 'Message',
+            icon: 'pi pi-fw pi-comment',
+            routerLink: ['/uikit/message'],
+          },
+          {
+            label: 'File',
+            icon: 'pi pi-fw pi-file',
+            routerLink: ['/uikit/file'],
+          },
+          {
+            label: 'Chart',
+            icon: 'pi pi-fw pi-chart-bar',
+            routerLink: ['/uikit/charts'],
+          },
+          {
+            label: 'Misc',
+            icon: 'pi pi-fw pi-circle',
+            routerLink: ['/uikit/misc'],
+          },
+        ],
+      },
+      {
+        label: 'Prime Blocks',
+        items: [
+          {
+            label: 'Free Blocks',
+            icon: 'pi pi-fw pi-eye',
+            routerLink: ['/blocks'],
+            badge: 'NEW',
+          },
+          {
+            label: 'All Blocks',
+            icon: 'pi pi-fw pi-globe',
+            target: '_blank',
+          },
+        ],
+      },
+      {
+        label: 'Utilities',
+        items: [
+          {
+            label: 'PrimeIcons',
+            icon: 'pi pi-fw pi-prime',
+            routerLink: ['/utilities/icons'],
+          },
+          {
+            label: 'PrimeFlex',
+            icon: 'pi pi-fw pi-desktop',
+            target: '_blank',
+          },
+        ],
+      },
+      {
+        label: 'Pages',
+        icon: 'pi pi-fw pi-briefcase',
+        items: [
+          {
+            label: 'Landing',
+            icon: 'pi pi-fw pi-globe',
+            routerLink: ['/landing'],
+          },
+          {
+            label: 'Auth',
+            icon: 'pi pi-fw pi-user',
+            items: [
+              {
+                label: 'Login',
+                icon: 'pi pi-fw pi-sign-in',
+                routerLink: ['/auth/login'],
+              },
+              {
+                label: 'Error',
+                icon: 'pi pi-fw pi-times-circle',
+                routerLink: ['/auth/error'],
+              },
+              {
+                label: 'Access Denied',
+                icon: 'pi pi-fw pi-lock',
+                routerLink: ['/auth/access'],
+              },
+            ],
+          },
+          {
+            label: 'Crud',
+            icon: 'pi pi-fw pi-pencil',
+            routerLink: ['/pages/crud'],
+          },
+          {
+            label: 'Timeline',
+            icon: 'pi pi-fw pi-calendar',
+            routerLink: ['/pages/timeline'],
+          },
+          {
+            label: 'Not Found',
+            icon: 'pi pi-fw pi-exclamation-circle',
+            routerLink: ['/notfound'],
+          },
+          {
+            label: 'Empty',
+            icon: 'pi pi-fw pi-circle-off',
+            routerLink: ['/pages/empty'],
+          },
+        ],
+      },
+    ];
   }
 
   showSubMenu(id: number = 0) {
