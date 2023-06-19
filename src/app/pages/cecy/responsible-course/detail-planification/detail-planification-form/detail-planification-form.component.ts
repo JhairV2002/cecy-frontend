09d371afae99c4230b3c0787d1fa9b11d2353d1a
Soy-Anderson <ajr.parra@yavirac.edu.ec>
2023-06-19 07:14:40 -0500

    fix

--- src/app/pages/cecy/responsible-course/detail-planification/detail-planification-form/detail-planification-form.component.ts
+++ src/app/pages/cecy/responsible-course/detail-planification/detail-planification-form/detail-planification-form.component.ts
@@ -1,11 +1,28 @@
 import { CourseService } from '@services/cecy-v1/course.service';
-import { Component, OnInit, OnDestroy, Output, EventEmitter, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Output,
+  EventEmitter,
+  Input,
+} from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 import { MessageService } from '@services/core';
 import { CatalogueModel } from '@models/cecy';
-import { ClassroomHttpService, CatalogueHttpService, PlanificationHttpService, DetailPlanificationHttpService } from '@services/cecy';
+import {
+  ClassroomHttpService,
+  CatalogueHttpService,
+  PlanificationHttpService,
+  DetailPlanificationHttpService,
+} from '@services/cecy';
 import { ActivatedRoute } from '@angular/router';
 import { ClassroomModel } from '@models/cecy-v1/classroom.model';
 import { DetailPlanModel } from '@models/cecy-v1/detailPlan.model';
@@ -13,16 +30,15 @@ import { DetailPlanModel } from '@models/cecy-v1/detailPlan.model';
 @Component({
   selector: 'app-detail-planification-form',
   templateUrl: './detail-planification-form.component.html',
-  styleUrls: ['./detail-planification-form.component.scss']
+  styleUrls: ['./detail-planification-form.component.scss'],
 })
 export class DetailPlanificationFormComponent implements OnInit {
   @Input() data: any;
-  @Input() planId: number=0;
-
-
+  @Input() planId: number = 0;
 
   @Output() dialogForm = new EventEmitter<boolean>();
-  private detailPlanification$ = this.detailPlanificationHttpService.detailPlanification$;
+  private detailPlanification$ =
+    this.detailPlanificationHttpService.detailPlanification$;
   private unsubscribe$ = new Subject<void>();
   public formDetailPlanification: FormGroup = this.newFormDetailPlanification;
   public progressBar: boolean = false;
@@ -32,9 +48,8 @@ export class DetailPlanificationFormComponent implements OnInit {
   public classrooms: ClassroomModel[] = [];
   public parallels: CatalogueModel[] = [];
   public planification$ = this.planificationHttpService.planification$;
-  planificationId: number= 0;
+  planificationId: number = 0;
   instructors: any;
-
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -45,18 +60,15 @@ export class DetailPlanificationFormComponent implements OnInit {
     public messageService: MessageService,
     private planificationHttpService: PlanificationHttpService,
     private courseService: CourseService
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
-    console.log('este es el id de horario',this.planId)
-    this.courseService.getDetailPlan(this.data.id).subscribe(
-      response => {
-        console.log('esto es lo que responde', response)
-        this.formDetailPlanification.patchValue(response);
-        console.log('formulario', this.formDetailPlanification.value);
-      }
-    )
+    console.log('este es el id de horario', this.planId);
+    this.courseService.getDetailPlan(this.data.id).subscribe((response) => {
+      console.log('esto es lo que responde', response);
+      this.formDetailPlanification.patchValue(response);
+      console.log('formulario', this.formDetailPlanification.value);
+    });
 
     this.loadDays();
     this.loadWorkdays();
@@ -82,14 +94,18 @@ export class DetailPlanificationFormComponent implements OnInit {
       observation: [null, Validators.maxLength(255)],
       startedTime: [null, Validators.required],
       instructorId: [null, Validators.required],
-      stateId: [null]
+      stateId: [null],
     });
   }
 
-
   onSubmit() {
-    this.formDetailPlanification.patchValue({ planificationCourseId: this.planId })
-    console.log('este es el form para actualizar', this.formDetailPlanification.value)
+    this.formDetailPlanification.patchValue({
+      planificationCourseId: this.planId,
+    });
+    console.log(
+      'este es el form para actualizar',
+      this.formDetailPlanification.value
+    );
     if (this.formDetailPlanification.valid) {
       if (this.idField.value) {
         this.updateDetailPlanification(this.formDetailPlanification.value);
@@ -104,85 +120,37 @@ export class DetailPlanificationFormComponent implements OnInit {
   storeDetailPlanification(detailPlanification: DetailPlanModel): void {
     detailPlanification.stateId = 96;
     this.progressBar = true;
-    this.courseService
-      .saveDetailPlan(detailPlanification)
-      .subscribe({
-        next: (data) => {
-          console.log(data);
-          this.messageService.successCourse(data);
-          this.progressBar = false;
-          this.dialogForm.emit(false);
-        },
-        error: (error) => {
-          console.log(error);
-          this.messageService.error(error);
-          this.progressBar = false;
-        },
-      });
+    this.courseService.saveDetailPlan(detailPlanification).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.messageService.successCourse(data);
+        this.progressBar = false;
+        this.dialogForm.emit(false);
+      },
+      error: (error) => {
+        console.log(error);
+        this.messageService.error(error);
+        this.progressBar = false;
+      },
+    });
   }
 
   updateDetailPlanification(detailPlanification: DetailPlanModel): void {
     this.progressBar = true;
-    this.courseService
-      .updateDetailPlan(detailPlanification)
-      .subscribe({
-        next: (data) => {
-          console.log(data);
-          this.messageService.successCourse(data);
-          this.progressBar = false;
-          this.dialogForm.emit(false);
-        },
-        error: (error) => {
-          console.log(error);
-          this.messageService.error(error);
-          this.progressBar = false;
-        },
-      });
+    this.courseService.updateDetailPlan(detailPlanification).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.messageService.successCourse(data);
+        this.progressBar = false;
+        this.dialogForm.emit(false);
+      },
+      error: (error) => {
+        console.log(error);
+        this.messageService.error(error);
+        this.progressBar = false;
+      },
+    });
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   isRequired(field: AbstractControl): boolean {
     return field.hasValidator(Validators.required);
@@ -231,9 +199,10 @@ export class DetailPlanificationFormComponent implements OnInit {
 
   loadDays() {
     this.courseService.getCatalogues('DAY').subscribe(
-      response => {
+      (response) => {
         this.days = response;
-      }, error => {
+      },
+      (error) => {
         this.messageService.error(error);
       }
     );
@@ -241,9 +210,10 @@ export class DetailPlanificationFormComponent implements OnInit {
 
   loadWorkdays() {
     this.courseService.getCatalogues('WORKDAY').subscribe(
-      response => {
+      (response) => {
         this.workdays = response;
-      }, error => {
+      },
+      (error) => {
         this.messageService.error(error);
       }
     );
@@ -251,9 +221,10 @@ export class DetailPlanificationFormComponent implements OnInit {
 
   loadClassrooms() {
     this.courseService.getClassrooms().subscribe(
-      response => {
+      (response) => {
         this.classrooms = response;
-      }, error => {
+      },
+      (error) => {
         this.messageService.error(error);
       }
     );
@@ -261,19 +232,18 @@ export class DetailPlanificationFormComponent implements OnInit {
 
   loadParallels() {
     this.courseService.getCatalogues('PARALLEL_NAME').subscribe(
-      response => {
+      (response) => {
         this.parallels = response;
-      }, error => {
+      },
+      (error) => {
         this.messageService.error(error);
       }
     );
   }
 
   loadInstructors() {
-    this.courseService.getInstructors().subscribe(
-      response => {
-        this.instructors= response
-      }
-    )
+    this.courseService.getInstructors().subscribe((response) => {
+      this.instructors = response;
+    });
   }
 }
