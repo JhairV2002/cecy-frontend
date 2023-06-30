5e7ac69b251b1a11b155589dd2217e8d5397e985
Soy-Anderson <ajr.parra@yavirac.edu.ec>
2023-06-30 12:30:54 -0500

    add logic for student and user general

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -6,11 +6,12 @@ import {
   HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import jwt_decode from 'jwt-decode';
 import { TokenService } from './../services/auth';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor(private tokenService: TokenService) { }
+  constructor(private tokenService: TokenService) {}
 
   intercept(
     request: HttpRequest<unknown>,
@@ -48,10 +49,18 @@ export class TokenInterceptor implements HttpInterceptor {
 
   private addToken(request: HttpRequest<unknown>) {
     let token: string | undefined = '';
-    if (token) {
-      token = this.tokenService.getToken();
-    } else {
-      token = this.tokenService.getToken();
+    const tokenUser = this.tokenService.getToken();
+    const tokenStudent = this.tokenService.getEstudianteToken();
+
+    if (tokenUser) {
+      const decode = jwt_decode(tokenUser) as { [key: string]: any };
+      if (decode['role']) {
+        console.log('se ejecuto 1 token user');
+        token = this.tokenService.getToken();
+      }
+    } else if (tokenStudent) {
+      console.log('se ejecuto 2 token student');
+      token = this.tokenService.getEstudianteToken();
     }
 
     if (token) {
