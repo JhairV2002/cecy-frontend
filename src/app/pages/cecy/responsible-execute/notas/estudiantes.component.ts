5d5cf3319edfc05397e3f77554f306cc944f91d8
unknown <you@example.com>
2023-07-04 13:09:43 -0500

    Alertas en campos

--- src/app/pages/cecy/responsible-execute/notas/estudiantes.component.ts
+++ src/app/pages/cecy/responsible-execute/notas/estudiantes.component.ts
@@ -12,6 +12,7 @@ import { NombreFilterPipe } from './filter.pipe';
 })
 export class EstudiantesComponent implements OnInit {
   nombreFiltrado: string = '';
+  
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
@@ -70,4 +71,21 @@ export class EstudiantesComponent implements OnInit {
     .pipe(map((res) => res.filter((it) => it.estudiantes != null)));
 
   matricula$ = this.estudianteService.obtenerEstudiantePorId(4);
-}
+
+
+  validarNumero(event: KeyboardEvent): void {
+    const input = event.key;
+    const currentValue = (event.target as HTMLInputElement).value.trim();
+    const minValue = 1;
+    const maxValue = 100;
+  
+    if (
+      (isNaN(Number(input)) && input !== 'ArrowUp' && input !== 'ArrowDown' && input !== 'Backspace') ||
+      (currentValue !== '' && (Number(currentValue) < minValue || Number(currentValue) > maxValue))
+    ) {
+      event.preventDefault();
+      alert('Solo se permiten números del 1 al 100. No se permiten letras, números negativos o campos vacíos.');
+    }
+  }
+  }
+
