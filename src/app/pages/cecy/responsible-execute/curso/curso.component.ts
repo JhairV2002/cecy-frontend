a2b345cb9aece563dbda2f7adf8f440831c61515
KatherineCollaguazo <kas.collaguazo@yavirac.edu.ec>
2023-07-04 09:54:56 -0500

    cambio de endpoint

--- src/app/pages/cecy/responsible-execute/curso/curso.component.ts
+++ src/app/pages/cecy/responsible-execute/curso/curso.component.ts
@@ -1,4 +1,4 @@
-// curso.component.ts
+/* // curso.component.ts
 import { Component, OnInit } from '@angular/core';
 import { Curso } from './curso';
 import { CursoService } from './curso.service';
@@ -55,4 +55,69 @@ export class CursoComponent implements OnInit {
   redireccionar(cursoId: number) {
     this.router.navigate(['cecy/responsible-execute/notas/estudiante', cursoId ]);
   }
+}
+ */
+import { Component, OnInit } from '@angular/core';
+import { Curso } from './curso';
+import { CursoService } from './curso.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '@services/auth/auth.service';
+
+@Component({
+  selector: 'app-curso',
+  templateUrl: './curso.component.html',
+})
+export class CursoComponent implements OnInit {
+  cursos: Curso[] = [];
+  cursosFiltrados: Curso[] = [];
+  filtroNombre: string = '';
+  ascendingOrder: boolean = true;
+
+  constructor(
+    private cursoService: CursoService,
+    private activateRouter: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
+    ) {}
+
+    ngOnInit(): void {
+      this.authService.getProfile().subscribe((user) => {
+        const instructorId = 8; // Asignar un valor predeterminado (en este caso, 0) si instructorId es undefined
+
+        this.cursoService.getCursosByInstructor(instructorId).subscribe((cursos) => {
+          // Filtrar los cursos por estado "aprobado"
+          this.cursos = cursos.filter((curso) => curso.planificationCourse.state === 'aprobado');
+        });
+      });
+    }
+
+  filtrarCursos(): void {
+    if (this.filtroNombre.trim() !== '') {
+      this.cursosFiltrados = this.cursos.filter(
+        (curso) =>
+          curso.planificationCourse.name
+            .toLowerCase()
+            .includes(this.filtroNombre.toLowerCase()) ||
+          curso.planificationCourse.codeCourse
+            .toLowerCase()
+            .includes(this.filtroNombre.toLowerCase())
+      );
+    } else {
+      this.cursosFiltrados = this.cursos;
+    }
+  }
+
+  sortCards() {
+    this.cursosFiltrados.sort((a, b) =>
+      this.ascendingOrder
+        ? a.planificationCourse.name.localeCompare(b.planificationCourse.name)
+        : b.planificationCourse.name.localeCompare(a.planificationCourse.name)
+    );
+    this.ascendingOrder = !this.ascendingOrder;
+  }
+
+  redireccionar(cursoId: number) {
+    this.router.navigate(['cecy/responsible-execute/notas/estudiante', cursoId ]);
+  }
+
 }
--- src/app/pages/cecy/responsible-execute/curso/curso.service.ts
+++ src/app/pages/cecy/responsible-execute/curso/curso.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+/* import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Curso } from './curso';
@@ -13,5 +13,27 @@ export class CursoService {
 
   getCursos(): Observable<Curso[]> {
     return this.http.get<Curso[]>(this.url);
+  }
+}
+ */
+
+// curso.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Curso } from './curso';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursoService {
+  private apiUrl = 'http://localhost:8080/api/courses/find/instructor/';
+
+  constructor(private http: HttpClient) {}
+
+  getCursosByInstructor(instructorId: number): Observable<Curso[]> {
+    const url = `${this.apiUrl}${instructorId}`;
+
+    return this.http.get<Curso[]>(url);
   }
 }
