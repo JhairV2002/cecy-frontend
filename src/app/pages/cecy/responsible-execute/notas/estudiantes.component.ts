4729d860177f5f22786a764049cb9ebf6b5cd5da
jsmcervantes <jsm.cervantes@yavirac.edu.ec>
2023-07-04 19:49:52 -0500

    boton generar excel

--- src/app/pages/cecy/responsible-execute/notas/estudiantes.component.ts
+++ src/app/pages/cecy/responsible-execute/notas/estudiantes.component.ts
@@ -5,12 +5,16 @@ import { Observable, filter, map, switchMap } from 'rxjs';
 import { EstudianteService } from './estudiante.service';
 import { Matriculas } from './estudiante.model';
 import { NombreFilterPipe } from './filter.pipe';
+import * as fs from 'fs';
+import * as XLSX from 'xlsx';
+
 @Component({
   selector: 'app-estudiantes',
   templateUrl: './estudiantes.component.html',
   styleUrls: ['./estudiantes.component.scss'],
 })
 export class EstudiantesComponent implements OnInit {
+ 
   nombreFiltrado: string = '';
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -78,4 +82,28 @@ export class EstudiantesComponent implements OnInit {
     .pipe(map((res) => res.filter((it) => it.estudiantes != null)));
 
   matricula$ = this.estudianteService.obtenerEstudiantePorId(4);
+
+
+ generarExcel(notas: Matriculas[]): void {
+  const datosExportar = notas.map(nota => {
+    const estado = nota.promedio >= 70 ? 'Aprobado' : 'Reprobado';
+
+    return {
+      Nota1: nota.nota1,
+      Nota2: nota.nota2,
+      Promedio: nota.promedio,
+      Estado: estado,
+    };
+  });
+
+  const libro = XLSX.utils.book_new();
+  const hoja = XLSX.utils.json_to_sheet(datosExportar);
+  XLSX.utils.book_append_sheet(libro, hoja, 'Notas');
+
+  const nombreArchivo = 'notas.xlsx';
+  XLSX.writeFile(libro, nombreArchivo);
+
+  console.log(`El archivo Excel "${nombreArchivo}" ha sido generado exitosamente.`);
+}
+
 }
