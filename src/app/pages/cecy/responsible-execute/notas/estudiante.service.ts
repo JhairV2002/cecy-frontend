d49910b989a233712d6f56ac9a60d387df468414
unknown <you@example.com>
2023-07-05 22:39:15 -0500

    Arreglo de Notas

--- src/app/pages/cecy/responsible-execute/notas/estudiante.service.ts
+++ src/app/pages/cecy/responsible-execute/notas/estudiante.service.ts
@@ -3,30 +3,28 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Matriculas } from './estudiante.model';
 import { environment } from '@env/environment';
-import * as fs from 'fs';
-//import * as XLSX from 'xlsx';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EstudianteService {
-  private apiUrl = `${environment.api}/matriculas/`;
-  constructor(private http: HttpClient) {}
+  private baseUrl = `${environment.api}/matriculas/`;
+  constructor(private http: HttpClient) { }
 
   obtenerEstudiantes(): Observable<Matriculas[]> {
-    return this.http.get<Matriculas[]>(`${this.apiUrl}`);
+    return this.http.get<Matriculas[]>(this.baseUrl);
   }
 
   guardarEstudiante(estudiante: any): Observable<any> {
-    return this.http.put<Matriculas>(`${this.apiUrl}/${estudiante.id}`, estudiante);
+    return this.http.put<Matriculas>(`${this.baseUrl}/${estudiante.id}`, estudiante);
   }
 
   obtenerEstudiantePorId(id: number): Observable<Matriculas> {
-    return this.http.get<Matriculas>(`${this.apiUrl}${id}/`);
+    return this.http.get<Matriculas>(`${this.baseUrl}${id}/`);
   }
 
   obtenerMatriculasPorCursoId(id: number): Observable<Matriculas[]> {
-    return this.http.get<Matriculas[]>(`${this.apiUrl}cursoId/${id}/`);
+    return this.http.get<Matriculas[]>(`${this.baseUrl}cursoId/${id}/`);
   }
 
   actualizarNotas(matricula: Matriculas, id: number): Observable<Matriculas> {
@@ -35,18 +33,14 @@ export class EstudianteService {
     if (promedio >= 70) {
       matricula.estadoCurso = { descripcion: 'aprobado' };
       matricula.promedio = promedio;
-      return this.http.put<Matriculas>(`${this.apiUrl}${id}/`, matricula);
+
+      return this.http.put<any>(`${this.baseUrl}${id}/`, matricula);
     } else {
 
       matricula.estadoCurso = { descripcion: 'reprobado' };
       matricula.promedio = promedio;
-      return this.http.put<Matriculas>(`${this.apiUrl}${id}/`, matricula);
+      
+      return this.http.put<any>(`${this.baseUrl}${id}/`, matricula);
     }
   }
-
-
-    }
-
-
-
-
+}
