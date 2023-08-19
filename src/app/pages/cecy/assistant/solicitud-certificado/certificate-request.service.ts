76c726f33c6e9dceed8995e1297df4455ee7b063
JhairV2002 <jag.vasquez@yavirac.edu.ec>
2023-08-19 00:54:44 -0500

    agrega botón de descargar certificado

--- src/app/pages/cecy/assistant/solicitud-certificado/certificate-request.service.ts
+++ src/app/pages/cecy/assistant/solicitud-certificado/certificate-request.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable,BehaviorSubject, finalize } from 'rxjs';
+import { Observable, BehaviorSubject, finalize } from 'rxjs';
 
 import { environment } from '@env/environment';
 import {
@@ -11,21 +11,18 @@ import {
   Sponsor,
   TipoCertificado,
   UpdateCode,
-  tipo, } from './certificate';
-
+  tipo,
+} from './certificate';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CertificateRequestService {
-  constructor(
-    private http: HttpClient
-    ) { }
-
+  constructor(private http: HttpClient) {}
 
   tipoCertificado: TipoCertificado = {
-    id: 0
-  }
+    id: 0,
+  };
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
@@ -38,55 +35,73 @@ export class CertificateRequestService {
   private loading = new BehaviorSubject<boolean>(true);
   public loading$: Observable<boolean> = this.loading.asObservable();
 
-
   public findById(id: number): Observable<Report> {
     return this.http.get<Report>(
       this.apiUrl + '/' + id + '/',
       this.httpOptions
     );
   }
-  public saveCertificate(generarCertificado:Codes, id: number) {
-    return this.http.put<Codes>(this.apiUrl1+"/"+id+"/", generarCertificado,{observe: 'response'});
-  }
 
-  public updateCode(generarCertificado:UpdateCode, id: number): Observable<UpdateCode> {
-    return this.http.patch<UpdateCode>(this.apiUrl1+"/"+id+"/", generarCertificado);
+  public findByMatriculaId(id: number): Observable<Codes> {
+    return this.http.get<Codes>(`${this.apiUrl1}/findByMatriculaId/${id}/`);
   }
-
-  public downloadCertificate(id: number) {
-    return this.http.get(this.apiUrl2+"/pdf/"+id+"/",{responseType:'blob'});
-  }
-
-  uploadFile(formData: FormData): Observable<any>{
-    return this.http.post(this.apiUrl3+'/media/subir', formData);
-  }
-
-  public subirfirma(firmas: any): Observable<any>{
-    return this.http.post(this.apiUrl3+'/firma/',firmas);
-  }
-
-  public findAllFirms(): Observable<Firms[]> {
-    return this.http.get<Firms[]>(
-      this.apiUrl3 + '/firma/',
-      this.httpOptions
+  public saveCertificate(generarCertificado: Codes, id: number) {
+    return this.http.put<Codes>(
+      this.apiUrl1 + '/' + id + '/',
+      generarCertificado,
+      { observe: 'response' }
     );
   }
 
-  patchReport(report: any, id: number){
-    return this.http.patch(this.apiUrl+'/'+id+'/',report)
+  public updateCode(
+    generarCertificado: UpdateCode,
+    id: number
+  ): Observable<UpdateCode> {
+    return this.http.patch<UpdateCode>(
+      this.apiUrl1 + '/' + id + '/',
+      generarCertificado
+    );
   }
 
-   postTypeCertificate(tipoCertificado:TipoCertificado){
-    return this.http.post(this.apiUrl3+'/tipo-certificado/',tipoCertificado,{observe: 'response'});
+  public downloadCertificate(id: number) {
+    return this.http.get(this.apiUrl2 + '/pdf/' + id + '/', {
+      responseType: 'blob',
+    });
+  }
+
+  uploadFile(formData: FormData): Observable<any> {
+    return this.http.post(this.apiUrl3 + '/media/subir', formData);
+  }
+
+  public subirfirma(firmas: any): Observable<any> {
+    return this.http.post(this.apiUrl3 + '/firma/', firmas);
+  }
+
+  public findAllFirms(): Observable<Firms[]> {
+    return this.http.get<Firms[]>(this.apiUrl3 + '/firma/', this.httpOptions);
+  }
+
+  patchReport(report: any, id: number) {
+    return this.http.patch(this.apiUrl + '/' + id + '/', report);
+  }
+
+  postTypeCertificate(tipoCertificado: TipoCertificado) {
+    return this.http.post(
+      this.apiUrl3 + '/tipo-certificado/',
+      tipoCertificado,
+      { observe: 'response' }
+    );
   }
 
   public findByIdCourse(id: number): Observable<Course> {
     this.loading.next(true);
-    return this.http.get<Course>(this.apiUrl4 +"/"+ id, this.httpOptions).pipe(
-      finalize(() => {
-        this.loading.next(false);
-      })
-    );
+    return this.http
+      .get<Course>(this.apiUrl4 + '/' + id, this.httpOptions)
+      .pipe(
+        finalize(() => {
+          this.loading.next(false);
+        })
+      );
   }
 
   getSponsors(): Observable<Sponsor[]> {
--- src/app/pages/estudiante/mis-cursos/mis-cursos-details/mis-cursos-details.component.ts
+++ src/app/pages/estudiante/mis-cursos/mis-cursos-details/mis-cursos-details.component.ts
@@ -4,6 +4,8 @@ import { Matricula } from '@models/cecy/estudiantes/carreras';
 import { CursosService } from '@services/cecy/cursos';
 import { PlanificationCourse } from '@models/cecy';
 import { Observable, map } from 'rxjs';
+import { CertificateRequestService } from 'src/app/pages/cecy/assistant/solicitud-certificado/certificate-request.service';
+import { Codes } from 'src/app/pages/cecy/assistant/solicitud-certificado/certificate';
 
 @Component({
   selector: 'app-mis-cursos-details',
@@ -13,17 +15,39 @@ import { Observable, map } from 'rxjs';
 export class MisCursosDetailsComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
-    private cursosService: CursosService
-  ) { }
+    private cursosService: CursosService,
+    private certificadoService: CertificateRequestService
+  ) {}
 
   matricula!: Matricula;
 
   curso!: Observable<PlanificationCourse>;
+
+  codigo: Codes | null = null;
 
   ngOnInit() {
     this.matricula = history.state;
     this.curso = this.cursosService
       .getCursoById(this.matricula.cursoId)
       .pipe(map((res) => res.planification));
+
+    this.certificadoService
+      .findByMatriculaId(this.matricula.id!)
+      .subscribe((res) => {
+        this.codigo = res;
+        console.log('certificado', this.codigo);
+      });
+  }
+
+  descargarCertificado() {
+    this.certificadoService
+      .downloadCertificate(this.codigo?.id!)
+      .subscribe((data) => {
+        let dowloadURL = window.URL.createObjectURL(data);
+        let link = document.createElement('a');
+        link.href = dowloadURL;
+        link.download = 'certificado.pdf';
+        link.click();
+      });
   }
 }
